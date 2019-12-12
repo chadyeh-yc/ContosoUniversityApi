79ced04188720678bff196b0fb8431c5549405ae
chadyeh <chadyeh.yc@outlook.com>
2019-12-13 07:25:50 +0800

    Add OfficeAssignmentsController, PeopleController

---
+++ ContosoUniversityApi/Controllers/OfficeAssignmentsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversityApi.Context;
+using ContosoUniversityApi.Models;
+
+namespace ContosoUniversityApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfficeAssignmentsController : ControllerBase
+    {
+        private readonly ContosoUniversityContext _context;
+
+        public OfficeAssignmentsController(ContosoUniversityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OfficeAssignments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OfficeAssignment>>> GetOfficeAssignments()
+        {
+            return await _context.OfficeAssignments.ToListAsync();
+        }
+
+        // GET: api/OfficeAssignments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OfficeAssignment>> GetOfficeAssignment(int id)
+        {
+            var officeAssignment = await _context.OfficeAssignments.FindAsync(id);
+
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+
+            return officeAssignment;
+        }
+
+        // PUT: api/OfficeAssignments/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOfficeAssignment(int id, OfficeAssignment officeAssignment)
+        {
+            if (id != officeAssignment.InstructorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(officeAssignment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OfficeAssignmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OfficeAssignments
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<OfficeAssignment>> PostOfficeAssignment(OfficeAssignment officeAssignment)
+        {
+            _context.OfficeAssignments.Add(officeAssignment);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (OfficeAssignmentExists(officeAssignment.InstructorId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetOfficeAssignment", new { id = officeAssignment.InstructorId }, officeAssignment);
+        }
+
+        // DELETE: api/OfficeAssignments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<OfficeAssignment>> DeleteOfficeAssignment(int id)
+        {
+            var officeAssignment = await _context.OfficeAssignments.FindAsync(id);
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+
+            _context.OfficeAssignments.Remove(officeAssignment);
+            await _context.SaveChangesAsync();
+
+            return officeAssignment;
+        }
+
+        private bool OfficeAssignmentExists(int id)
+        {
+            return _context.OfficeAssignments.Any(e => e.InstructorId == id);
+        }
+    }
+}
